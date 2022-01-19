617d1840e82cea7a5bdd5dd84165cb1be0adb230
DJ Stelmach <dj@dstelmach.com>
2022-01-18 22:56:49 -0500

    install.sh install wheel before installing other dependencies

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 echo "Creating virtual environment (media-processor/venv)"
 python3 -m venv media-processor/venv
 echo "Created virtual environment"
+echo "Installing wheel"
+media-processor/venv/bin/pip3 install wheel
 echo "Installing pip dependencies..."
 media-processor/venv/bin/pip3 install -r media-processor/requirements.txt
 echo "Installed. You can start using Media Processor!"
