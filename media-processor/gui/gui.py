31b01e069090bd6f6901f502e2357a28ad39419b
DJ Stelmach <dj@dstelmach.com>
2022-01-25 19:43:35 -0500

    convert add edit property from function to class

--- media-processor/gui/gui.py
+++ media-processor/gui/gui.py
@@ -32,43 +32,94 @@ def get_destinations(lconn: LockableSqliteConn, user_at_ip: Optional[str] = None
 			lconn.cur.execute('''SELECT * FROM destination_servers;''')
 			return lconn.cur.fetchall()
 
-def add_edit_property(lconn: LockableSqliteConn, root: Tk, listbox: Listbox) -> None:
-	add_edit_window = Toplevel()
-	add_edit_window.minsize(500, 500)
-	#add_edit_window.grid_columnconfigure(0, weight=1)
-	#add_edit_window.grid_rowconfigure(0, weight=1)
 
-	Label(add_edit_window, text='Property').grid(row=0, column=0, columnspan=2)
-	property_entry = Entry(add_edit_window)
-	property_entry.grid(row=1, column=1)
-	pattern_entry = Entry(add_edit_window)
-	pattern_entry.grid(row=2, column=1)
-	partial_var = IntVar()
-	partial_checkbox = Checkbutton(add_edit_window, text='Partial?', variable=partial_var)
-	partial_checkbox.grid(row=3, column=0, columnspan=2)
-	Label(add_edit_window, text='Settings').grid(row=4, column=0, columnspan=2)
-	ffmpeg_args_entry = Entry(add_edit_window)
-	ffmpeg_args_entry.grid(row=5, column=1)
-	output_container_entry = Entry(add_edit_window)
-	output_container_entry.grid(row=6, column=1)
-	folder_entry = Entry(add_edit_window)
-	folder_entry.grid(row=7, column=1)
-	with lconn:
-		lconn.cur.execute('''SELECT user_at_ip FROM destination_servers;''')
-		user_at_ips = list(map(lambda uai: uai[0], lconn.cur.fetchall()))
-	destination_server_box = ttk.Combobox(add_edit_window, values=user_at_ips)
-	destination_server_box.grid(row=8, column=1)
-	is_show_var = IntVar()
-	is_show_checkbox = Checkbutton(add_edit_window, text='Is Show?', variable=is_show_var)
-	is_show_checkbox.grid(row=9, column=0, columnspan=2)
-	season_override_entry = Entry(add_edit_window)
-	season_override_entry.grid(row=10, column=1)
+class AddEditPropertyWindow:
+	def __init__(self, lconn: LockableSqliteConn, root: Tk, listbox: Listbox) -> None:
+		self.lconn = lconn
+		self.root = root
+		self.listbox = listbox
 
-	selected_property: Optional[str] = listbox.get(listbox.curselection()[0]) if len(listbox.curselection()) > 0 else None
-	if selected_property:
-		pass
-	else:
-		pass
+		# Build window
+		self.add_edit_window = Toplevel()
+		self.add_edit_window.minsize(500, 500)
+		self.add_edit_window.grid_columnconfigure(0, weight=1)
+		self.add_edit_window.grid_rowconfigure(0, weight=1)
+
+		main_frame = Frame(self.add_edit_window)
+		main_frame.grid(row=0, column=0, sticky=N)
+
+		property_frame = Frame(main_frame)
+		property_frame.grid(row=0, column=0)
+		Label(property_frame, text='Property').grid(row=0, column=0, columnspan=2)
+		Label(property_frame, text='Property:').grid(row=1, column=0)
+		self.property_entry = Entry(property_frame)
+		self.property_entry.grid(row=1, column=1)
+		Label(property_frame, text='Pattern:').grid(row=2, column=0)
+		self.pattern_entry = Entry(property_frame)
+		self.pattern_entry.grid(row=2, column=1)
+		self.partial_var = IntVar()
+		partial_checkbox = Checkbutton(property_frame, text='Partial?', variable=self.partial_var)
+		partial_checkbox.grid(row=3, column=0, columnspan=2)
+
+		settings_frame = Frame(main_frame)
+		settings_frame.grid(row=3, column=0)
+		Label(settings_frame, text='Settings').grid(row=0, column=0, columnspan=2)
+		Label(settings_frame, text='FFMPEG Args:').grid(row=1, column=0)
+		self.ffmpeg_args_entry = Entry(settings_frame)
+		self.ffmpeg_args_entry.grid(row=1, column=1)
+		Label(settings_frame, text='Output Container:').grid(row=2, column=0)
+		self.output_container_entry = Entry(settings_frame)
+		self.output_container_entry.grid(row=2, column=1)
+		Label(settings_frame, text='Folder:').grid(row=3, column=0)
+		self.folder_entry = Entry(settings_frame)
+		self.folder_entry.grid(row=3, column=1)
+		Label(settings_frame, text='Destination Server (leave blank for local):').grid(row=4, column=0)
+		with self.lconn:
+			self.lconn.cur.execute('''SELECT user_at_ip FROM destination_servers;''')
+			user_at_ips = list(map(lambda uai: uai[0], self.lconn.cur.fetchall()))
+		self.destination_server_box = ttk.Combobox(settings_frame, values=user_at_ips)
+		self.destination_server_box.grid(row=4, column=1)
+		self.is_show_var = IntVar()
+		is_show_checkbox = Checkbutton(settings_frame, text='Is Show?', variable=self.is_show_var)
+		is_show_checkbox.grid(row=5, column=0, columnspan=2)
+		Label(settings_frame, text='Season Override (leave blank for auto):').grid(row=6, column=0)
+		self.season_override_entry = Entry(settings_frame)
+		self.season_override_entry.grid(row=6, column=1)
+
+		action_frame = Frame(self.add_edit_window)
+		action_frame.grid(row=4, column=0, sticky=SE)
+		Button(action_frame, text='Save and Exit', command=self.save_and_exit).grid(row=0, column=0, padx=5)
+
+	def get_settings(self) -> dict:
+		'''Get the settings as a dict of dicts representing the DB entries'''
+		prop = self.property_entry.get()
+		return {
+			'properties': {
+				'property': prop,
+				'pattern': self.pattern_entry.get(),
+				'partial': self.partial_var.get()
+			},
+			'settings': {
+				'property': prop,
+				'ffmpeg_args': self.ffmpeg_args_entry.get(),
+				'output_container': self.output_container_entry.get(),
+				'folder': self.folder_entry.get(),
+				'user_at_ip': self.destination_server_box.get(),
+				'is_show': self.is_show_var.get(),
+				'season_override': self.season_override_entry.get()
+			}
+		}
+
+	def save_and_exit(self) -> None:
+		'''Save changes and close TopLevel'''
+		# TODO: save settings to db
+		self.add_edit_window.destroy()
+
+	# selected_property: Optional[str] = listbox.get(listbox.curselection()[0]) if len(listbox.curselection()) > 0 else None
+	# if selected_property:
+	# 	pass
+	# else:
+	# 	pass
 
 def remove_propery(lconn: LockableSqliteConn, root: Tk, listbox: Listbox) -> None:
 	pass
@@ -104,7 +155,7 @@ def driver(lconn: LockableSqliteConn) -> None:
 	property_listbox_scroll.config(command=property_listbox.yview)
 	property_actions_frame = Frame(main_frame)
 	property_actions_frame.grid(row=0, column=1)
-	Button(property_actions_frame, text='Add/Edit property', command=lambda: add_edit_property(lconn, root, property_listbox)).grid(row=0, column=0)
+	Button(property_actions_frame, text='Add/Edit property', command=lambda: AddEditPropertyWindow(lconn, root, property_listbox)).grid(row=0, column=0)
 	Button(property_actions_frame, text='Remove property', command=lambda: remove_propery(lconn, root, property_listbox)).grid(row=1, column=0)
 
 	for i, row in enumerate(get_properties(lconn)):
